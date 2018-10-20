07e58b424e27a2f7421a7eb24133329961ae5bdd
ramyaprasad63 <44326267+ramyaprasad63@users.noreply.github.com>
2018-10-20 16:13:05 -0500

    Add files via upload

--- hunger.py
+++ hunger.py
@@ -12,15 +12,21 @@ import sys
 import re
 
 def Main():
-    countyhungerdata = pd.read_excel('foodinsecurity.xlsx', sheet_name = 'CountyData')
-    #countyhungerdata = CountyData(countyhungerdata)
+    excel_data = 'foodinsecurity.xlsx'
+    #countyhungerdata = pd.read_excel('foodinsecurity.xlsx', sheet_name = 'CountyData')
+    countyhungerdata = CountyData(excel_data, 'CountyData')
     pprint(countyhungerdata)
-    #pprint(countyhungerdata)
 
 
-
-#def CountyData(countydatadf):
-    #return(countydatadf)
+def CountyData(excel_data, sheet_name):
+    columns = ['County', 'Population', 'Rate', 'Individuals', '% below 165% poverty', '% between 165% and 185% poverty', '% above 185% poverty']
+    countydatadf =pd.read_excel(excel_data, sheet_name = sheet_name, names = columns)
+    countydatadf.drop(['% below 165% poverty', '% between 165% and 185% poverty', '% above 185% poverty'], axis = 1, inplace = True)
+    countydatadf.drop([254], axis = 0, inplace = True)
+    countydatadf.sort_values(['Individuals'],ascending = False, inplace = True)
+    countydatadf.reset_index(drop = True, inplace = True)
+    
+    return(countydatadf)
     
 if __name__== "__main__":
   Main()
